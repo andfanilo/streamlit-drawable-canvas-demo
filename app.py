6b5f6371375b7dc404d4510f094359dd68bdab8c
Fanilo Andrianasolo <andfanilo@gmail.com>
2022-02-26 08:21:32 +0100

    Prepare 0.9.0: add new point demo

--- app.py
+++ app.py
@@ -82,14 +82,16 @@ def full_app():
 
     with st.echo("below"):
         # Specify canvas parameters in application
+        drawing_mode = st.sidebar.selectbox(
+            "Drawing tool:",
+            ("freedraw", "line", "rect", "circle", "transform", "polygon", "point"),
+        )
         stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+        if drawing_mode == 'point':
+            point_display_radius = st.sidebar.slider("Point display radius: ", 1, 25, 3)
         stroke_color = st.sidebar.color_picker("Stroke color hex: ")
         bg_color = st.sidebar.color_picker("Background color hex: ", "#eee")
         bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
-        drawing_mode = st.sidebar.selectbox(
-            "Drawing tool:",
-            ("freedraw", "line", "rect", "circle", "transform", "polygon"),
-        )
         realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
         # Create a canvas component
@@ -102,6 +104,7 @@ def full_app():
             update_streamlit=realtime_update,
             height=150,
             drawing_mode=drawing_mode,
+            point_display_radius=point_display_radius if drawing_mode == 'point' else 0,
             display_toolbar=st.sidebar.checkbox("Display toolbar", True),
             key="full_app",
         )
